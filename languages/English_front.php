ecd1b238eee9ebd3a1b6cbc56dd67366a306a8f0
René Schlegel <rene.schlegel@sendto.ch>
2016-05-19 16:46:12 +0200

    change language

---
+++ languages/English_front.php
@@ -0,0 +1,89 @@
+<?php
+/*
++---------------------------------------------------------------+
+|     Turnier-System for e107 v0.7+ and v0.8+
+|        
+|     a plugin for the e107 website system
+|     http://www.e107.org/
+|
+|     © schlrech
+|     rene.schlegel@sendto.ch
++---------------------------------------------------------------+
+*/
+
+define(LAN_EVENT_01,'Upcoming Events');
+define(LAN_EVENT_02,' findet am ');
+define(LAN_EVENT_02a,' statt.');
+define(LAN_EVENT_03,'Klicke um den Event anzuschauen');
+define(LAN_EVENT_04,'Event overview');
+define(LAN_EVENT_05,'Datum');
+define(LAN_EVENT_06,'Event wirklich löschen?');
+define(LAN_EVENT_07,'Event');
+define(LAN_EVENT_08,'Anmeldeschluss');
+define(LAN_EVENT_09,'Freie Plätze');
+define(LAN_EVENT_10,'Action');
+define(LAN_EVENT_11,'Event Anmelden');
+define(LAN_EVENT_12,'Event Abmeldung');
+define(LAN_EVENT_13,'Event bearbeiten');
+define(LAN_EVENT_14,'Teilnehmer');
+define(LAN_EVENT_15,'delete Event');
+define(LAN_EVENT_16,'Anzahl Events: ');
+define(LAN_EVENT_17,'Event Detail');
+define(LAN_EVENT_18,'Am Event anmelden');
+define(LAN_EVENT_19,'Am Event Abmeldung');
+define(LAN_EVENT_20,'Neuen Event erstellen');
+define(LAN_EVENT_21,'Du musst dich einloggen oder registrieren um Events zu sehen.');
+define(LAN_EVENT_22,'Neuer Event');
+define(LAN_EVENT_23,'Event speichern');
+define(LAN_EVENT_24,'Event aktualisieren');
+define(LAN_EVENT_25,'Event Name:');
+define(LAN_EVENT_26,'Event Date:');
+define(LAN_EVENT_27,'max. Plätze:');
+define(LAN_EVENT_28,'Cancel');
+define(LAN_EVENT_29,'Anmeldeschluss');
+define(LAN_EVENT_30,'Anmeldeschluss kann nicht nach dem Event Datum sein');
+define(LAN_EVENT_31,'Event Details:');
+define(LAN_EVENT_32,'Bitte gebe einen Event-Name an');
+define(LAN_EVENT_33,'Bitte gib ein Korrektes Datum an (dd.mm.yyyy)');
+define(LAN_EVENT_34,'Das Datum liegt in der Vergangenheit');
+define(LAN_EVENT_35,'Event wurde erfolgreich gespeichert');
+define(LAN_EVENT_36,'Der Event konnte nicht gespeichert werden');
+define(LAN_EVENT_37,'Event: ');
+define(LAN_EVENT_38,'von');
+define(LAN_EVENT_39,'Mitglieder haben sich für den Event');
+define(LAN_EVENT_40,'angemeldet:');
+define(LAN_EVENT_41,'Anzahl freie Plätze:');
+define(LAN_EVENT_42,'Event Manager:');
+define(LAN_EVENT_42a,'Event Manager Logs:');
+define(LAN_EVENT_43,'Sorry du hast den Anmeldeschluss verpasst.');
+define(LAN_EVENT_44,'Du kannst dich nicht mehr abmelden. Die Anmeldung ist verbindlich!');
+define(LAN_EVENT_45,'Am Event anmelden');
+define(LAN_EVENT_46,'Du bist für diesen Event angemeldet.');
+define(LAN_EVENT_47,'Vom Event abmelden');
+define(LAN_EVENT_48,'Die maximale Anzahl an Plätzen ist schon gebucht');
+define(LAN_EVENT_49,'Event erfolgreich gebucht');
+define(LAN_EVENT_50,'Vom Event abgemeldet');
+define(LAN_EVENT_51,'Event Übersicht');
+define(LAN_EVENT_52,'Auch vergangene Events anzeigen');
+define(LAN_EVENT_53,'Nur aktuelle Events anzeigen');
+define(LAN_EVENT_54,'Teilnehmer abmelden');
+define(LAN_EVENT_55,'Teilnehmer wählen');
+define(LAN_EVENT_56,'Teilnehmer anmelden');
+define(LAN_EVENT_57,'Mitglied %s hat versucht sich am Event %s abzumelden obwohl dies nicht mehr möglich ist');
+define(LAN_EVENT_58,'Mitglied %s hat versucht sich am Event %s nach dem Anmeldeschluss noch anzumelden');
+define(LAN_EVENT_59,'EventMgr');
+define(LAN_EVENT_60,'Als Admin doch gespeichert');
+define(LAN_EVENT_61,'Alle Events anzeigen');
+define(LAN_EVENT_62,'Termin in Kalender eintragen');
+define(LAN_EVENT_63,'Event Location:');
+define(LAN_EVENT_64,'Event costs:');
+define(LAN_EVENT_65,'Event created by:');
+define(LAN_EVENT_66,'Neuer oder geänderter Event auf WhatYouWant');
+define(LAN_EVENT_67,'Event Details');
+define(LAN_EVENT_68,'send Event to all');
+define(LAN_EVENT_69,'Event Mails wurden in die Warteschlange gestellt. Dauer ca. 2 Stunden bis zum Versand');
+define(LAN_EVENT_70,'Event Mails versenden');
+define(LAN_EVENT_71,'Liebe Grüsse und bis bald');
+define(LAN_EVENT_72,'Conny & René');
+
+?>
--- languages/German_global.php
+++ languages/German_global.php
@@ -1,4 +1,4 @@
 <?php
 define("LAN_PLUGIN_EVENTMANAGER_NAME", "Event Manager");
-define("LAN_PLUGIN_EVENTMANAGER_DESCRIPTION", "Diese Plugin ist ein Event Manager.");
+define("LAN_PLUGIN_EVENTMANAGER_DESCRIPTION", "Event Manager.");
 ?>
